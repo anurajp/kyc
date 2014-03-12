eade4f394d80377b1bc63c1893f69f7a8a1ba5e6
anurajp <anuraj.kool@gmail.com>
2014-03-13 01:55:14 +0530

    Commit header.php to resolve sync

--- application/views/header.php
+++ application/views/header.php
@@ -1,142 +1,155 @@
-<html>
+<!DOCTYPE html>
+<!-- 
+    Free Responsive Template by templatemo
+    http://www.templatemo.com
+-->
+<html lang="en">
 <head>
-    <meta charset=”utf-8”>
-    <title>
-        Welcome to KYC
-    </title>
-    </head>
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-<!-- Optional theme -->
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css"></head>
-<style type="text/css">
+    <title>Know your candidate</title>
+    <meta name="keywords" content="urbanic, responsive, bootstrap, fluid layout, orange, white, free website template, templatemo" />
+    <meta name="description" content="Urbanic is free responsive template using Bootstrap which is compatible with mobile devices. This layout is provided by templatemo for free of charge." />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link rel="shortcut icon" href="PUT YOUR FAVICON HERE">-->
+    <!-- Bootstrap core CSS -->
+    <link href="../../kyc/application/views/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="../../kyc/application/views/css/bootstrap-theme.min.css" rel="stylesheet">
 
-    #search-box {
-        position: relative;
-        width: 100%;
-        margin: 0;
-    }
 
-    #search-form {
-        height: 40px;
-        border: 1px solid #999;
-        -webkit-border-radius: 5px;
-        -moz-border-radius: 5px;
-        border-radius: 5px;
-        background-color: #fff;
-        overflow: hidden;
-    }
+    <!-- Bootstrap core CSS -->
+    <link href="../../kyc/application/views/css/app.css" rel="stylesheet">
 
-    #search-text {
-        font-size: 14px;
-        color: #ddd;
-        border-width: 0;
-        background: transparent;
-    }
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../css/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
 
-    #search-box input[type="text"] {
-        width: 1000px;
-        padding: 11px 0 12px 1em;
-        color: #333;
-        outline: none;
-    }
-
-    #search-button {
-        position: absolute;
-        top: 0;
-        right: 0;
-        height: 42px;
-        width: 80px;
-        font-size: 14px;
-        color: #fff;
-        text-align: center;
-        line-height: 42px;
-        border-width: 0;
-        background-color: #999;
-        -webkit-border-radius: 0px 5px 5px 0px;
-        -moz-border-radius: 0px 5px 5px 0px;
-        border-radius: 0px 5px 5px 0px;
-        cursor: pointer;
-    }
-
-    /* Comparision Chart */
-    #price-comparison {
-        border-collapse: collapse;
-        width: 10       0%;
-    }
-
-    #comparison-image-cell {
-        width: 60px;
-    }
-
-    .spacer {
-        background: #e4e4e4;
-        border: 1px solid #e4e4e4;
-        height: 10px;
-    }
-
-    #comparison-header {
-        background: #e4e4e4;
-        border: 1px solid #e4e4e4;
-        height: 30px;
-    }
-
-    #price-comparison td {
-        border: 1px solid #e4e4e4;
-        padding: 5px;
-        text-align: center;
-    }
-
-    .merchant-image {
-        height: 50px;
-        width: 50px;
-        background: #fff;
-        padding: 2px;
-        border: 1px solid #e4e4e4;
-    }
-
-    .merchant-name-td {
-        font-size: 18px;
-        font-weight: bold;
-    }
-
-    /* Sidebar */
-    #sidebar {
-        margin-top: 0px;
-        width: 300px;
-        float: right;
-    }
+    <!-- Google Web Font Embed -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
 
 
 
-</style>
+    <!-- Custom styles for this template -->
+    <link href="../../kyc/application/views/js/colorbox/colorbox.css"  rel='stylesheet' type='text/css'>
+    <link href="../../kyc/application/views/css/templatemo_style.css"  rel='stylesheet' type='text/css'>
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <![endif]-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="../../kyc/application/views/js/bootstrap.min.js"  type="text/javascript"></script>
+
+
+    <script>
+        $(document).ready(function(){
+            $("#search_field").keyup(function(){ <!-- On type, call server. If output not empty add content to search table and unhide-->
+
+                $("#search_results").html();
+
+                var key = $("#search_event_type").val();
+                var val = $("#search_field").val();
+
+                if(val.length == 0) {
+                    $("#search_results").hide();
+                    return;
+                }
+
+                var url = "http://localhost/kyc/index.php/games/render_drop_down_search/Loksabha/" + key + "/" + val;
+
+                $.get(url, function(data,status){
+
+                    if(status == "success" && data.length != 0) {
+                        $("#search_results").html(data);
+                        $("#search_results").show();
+                    } else {
+                        $("#search_results").hide();
+                    }
+                });
+            });
+        });
+
+    </script>
+
+
 </head>
+
 <body>
-<div class="container">
-    <!-- header content -->
-    <h1><a href="/kyc/index.php/hello/one">Know Your Candidate</a></h1>
-    <!-- Navigation Bar Contains Home -->
-    <div class = "navbar navbar-inverse navbar-static-top">
-        <div class = "container">
+
+<div class="templatemo-top-bar" id="templatemo-top">
+    <div class="container">
+        <!--<div class="subheader">
+
+        </div> -->
+        <div class="subheader">
+            <div id="phone" class="pull-left">
+                <img src="images/phone.png" alt="phone"/>
+                +91-8105719832
+            </div>
+            <div id="email" class="pull-right">
+                <img src="images/email.png" alt="email"/>
+                contact@website.com
+            </div>
+        </div>
+    </div>
+</div>
+<div class="templatemo-top-menu">
 
 
 
-            <button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
-                <span class = "icon-bar"></span>
-                <span class = "icon-bar"></span>
-                <span class = "icon-bar"></span>
-            </button>
 
-            <div class = "collapse navbar-collapse navHeaderCollapse">
+    <div class="container">
 
-                <ul class = "nav navbar-nav navbar-left ">
+        <div class="form-group search-form" >
+            <form role="form">
+                <select id="search_event_type" class="">
+                    <option value="cprefix">Candidate</option>
+                    <option value="location">Location</option>
+                    <option value="city">City</option>
+                    <option value="state">State</option>
+                    <option value="zipcode">Zipcode</option>
+                </select>
 
-                    <li class = "active"><a href = "/kyc/index.php/hello/one">Home</a></li>
-                    <li><a href = "#">About</a></li>
-                    <li><a href = "/kyc/index.php/hello/contactUs">Contact Us</a></li>
-
-                </ul>
-
+                <input id="search_field" class="col-lg-8" type="text" placeholder="Type here" autocomplete="off">
+            </form>
+            <div id="search_results" hidden="hidden" class="search_results">
             </div>
 
         </div>
-    </div>
+
+        <!-- Static navbar -->
+        <div class="navbar navbar-default" role="navigation">
+            <div class="container">
+
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="" rel="nofollow"><img src="images/kyc-logo.png" alt="logo"/></a>
+                </div>
+                <div class="navbar-collapse collapse" id="templatemo-nav-bar">
+                    <ul class="nav navbar-nav navbar-right" style="margin-top: 40px;">
+                        <li><a href="#templatemo-top">HOME</a></li>
+                        <li><a href="#templatemo-about">ABOUT</a></li>
+                        <li><a href="#templatemo-portfolio">PORTFOLIO</a></li>
+                        <li><a href="#templatemo-blog">BLOG</a></li>
+                        <li><a href="#templatemo-contact">CONTACT</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+
+            </div><!--/.container-fluid -->
+
+        </div><!--/.navbar -->
+
+
+    </div> <!-- /container -->
+</div>
+
+
+
+
