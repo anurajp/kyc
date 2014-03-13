7d986afeac3c72e894261e25314bc74e817fd255
anurajp <anuraj.kool@gmail.com>
2014-03-14 02:14:58 +0530

    Deleting unused file

--- application/views/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div id="sidebar">
-    <a class="twitter-timeline" href="https://twitter.com/bansal_ankur" data-widget-id="442678504816005120">Tweets by @bansal_ankur</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
-    <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FAAPkaArvind&amp;width&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=true&amp;appId=546431202104656" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:258px;" allowTransparency="true"></iframe>
-</div>
-<div>
-
-</div>
