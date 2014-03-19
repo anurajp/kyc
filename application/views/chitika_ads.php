ff042cdf65631c6a10c0797bca1e462f6a9636c1
anurajp <anuraj.kool@gmail.com>
2014-03-19 13:22:57 +0530

    Chitika and info link file add

---
+++ application/views/chitika_ads.php
@@ -0,0 +1,19 @@
+<div id="chitika_ads">
+    <div class="container">
+        <div>
+            <script type="text/javascript">
+                ( function() {
+                    if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
+                    var unit = {"publisher":"anurajp","width":1000,"height":120,"sid":"Chitika Default"};
+                    var placement_id = window.CHITIKA.units.length;
+                    window.CHITIKA.units.push(unit);
+                    document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
+                    var s = document.createElement('script');
+                    s.type = 'text/javascript';
+                    s.src = '//cdn.chitika.net/getads.js';
+                    try { document.getElementsByTagName('head')[0].appendChild(s); } catch(e) { document.write(s.outerHTML); }
+                }());
+            </script>
+        </div>
+    </div>
+</div>
---
+++ application/views/infolinks.php
@@ -0,0 +1,9 @@
+<div id="kyc-ads">
+    <div class="container">
+        <script type="text/javascript">
+            var infolinks_pid = 2060295;
+            var infolinks_wsid = 0;
+        </script>
+        <script type="text/javascript" src="http://resources.infolinks.com/js/infolinks_main.js"></script>
+    </div>
+</div>
